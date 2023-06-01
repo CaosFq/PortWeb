97562f6740f7d80d496c33409bcf327bf298f624
CaosFq <andresromerofq@gmail.com>
2023-06-01 18:45:09 +0200

    first

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 //Modules
-const express = require('express');
+const express = require("express");
 const app = express();
-const router = require('../routes/index');
-const path = require('path');
+const router = require("../routes/index");
+const path = require("path");
 
 //Settings
-app.set('port', 3001);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
+app.set("port", 3007);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
 //Midlwares
 app.use(router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //Server Listening
-app.listen(app.get('port'), () => {
-  console.log(`Server Runnig on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server Runnig on port ${app.get("port")}`);
 });
 module.exports = app;
